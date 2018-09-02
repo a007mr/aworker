aabcb50272e59944122c0df53a5209e0106901a3
Scam08 <spaun08_@mail.ru>
2018-09-02 17:59:00 +0300

    Update

--- testnet/networkStart.bat
+++ testnet/networkStart.bat
@@ -1,1 +1,1 @@
-geth --datadir "/testnet" --port "30306" --identity "aworkeryes" --networkid 1999 --rpc --rpcaddr 0.0.0.0 --rpcport 8546 --rpccorsdomain "*" --rpcapi "db,eth,net,personal,web3" console
+geth --datadir "/testnet" --port "30306" --identity "aworkeryes" --networkid 1999 --syncmode=fast --cache=1024 --rpc --rpcaddr 0.0.0.0 --rpcport 8546 --rpccorsdomain "*" --rpcapi "db,eth,net,personal,web3" console
